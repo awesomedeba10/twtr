2268382cf8b15f89b5b4c3eef88b04a748fc1fdc
awesomedeba10 <officialdeba10@gmail.com>
2022-02-27 10:07:14 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='twtr',
+    version='0.1',
+    description='Done under the AlmaBetter Assignment to process Twitter data and make this ready for modeling',
+    author='Debanjan Ganguly',
+    author_email='officialdeba10@gmail.com',
+    url='',
+    packages=['twtr'],
+    license='MIT',
+)
